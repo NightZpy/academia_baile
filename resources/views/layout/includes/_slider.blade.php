a49621cab694eaf749327e9644298717cb713ddb
Lenyn Alcántara <nightzpy@gmail.com>
2015-12-12 05:53:24 +0000

    arreglo slider para registro o ingreso a pluranza

--- resources/views/layout/includes/_slider.blade.php
+++ resources/views/layout/includes/_slider.blade.php
@@ -1,8 +1,15 @@
 <div class="ct-u-owlWrapper">
     <div class="ct-shadowBox-repositioned">
-        <div class="ct-shadowBox ct-backgroundContent" data-type="color" data-bg-color="rgba(255, 255, 255, .4)" data-snap-ignore="true">
-            @include('pluranza.academies-participants.partials._base-data-form')
-        </div>
+        @if(!Auth::user())
+            <div class="ct-shadowBox ct-backgroundContent" data-type="color" data-bg-color="rgba(255, 255, 255, .4)" data-snap-ignore="true">
+                @include('pluranza.academies-participants.partials._base-data-form')
+            </div>
+        @endif
+        @if(Auth::user()->academieParticipant)
+            <div class="ct-shadowBox ct-backgroundContent">
+                <h1>Ingresar a sitio de PLURANZA</h1>
+            </div>
+        @endif
     </div>
     <!-- Main Carousel -->
     <div class="ct-js-owl ct-u-owl ct-mainCarousel ct-mainCarousel--arrowsMiddle" data-single="true" data-height="844" data-animations="true" data-navigation="true" data-pagination="false" data-bg="true" data-snap-ignore="true">
