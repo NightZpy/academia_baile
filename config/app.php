80a9916dc13322c828516b7f1b2d60ac970616a7
Lenyn Alcántara <nightzpy@gmail.com>
2015-12-17 03:21:37 +0000

    Chumper datatable agregado

--- config/app.php
+++ config/app.php
@@ -153,7 +153,7 @@ return [
         Laracasts\Flash\FlashServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
         Codesleeve\LaravelStapler\Providers\L5ServiceProvider::class,
-        Yajra\Datatables\DatatablesServiceProvider::class
+        Chumper\Datatable\DatatableServiceProvider::class,
     ],
 
     /*
@@ -205,7 +205,7 @@ return [
         'Flash'     => Laracasts\Flash\Flash::class,
         'Html'      => Collective\Html\HtmlFacade::class,
         'Form'      => Collective\Html\FormFacade::class,
-        'Datatables' => Yajra\Datatables\Datatables::class
+        'Datatable' => 'Chumper\Datatable\Facades\DatatableFacade',
     ],
 
 ];
