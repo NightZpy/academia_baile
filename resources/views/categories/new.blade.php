951f06cae86e063a5784d570c874088eedda7551
Lenyn Alcántara <nightzpy@gmail.com>
2015-12-18 19:54:17 +0000

    Agregada vista para nueva categoría

--- resources/views/categories/new.blade.php
+++ resources/views/categories/new.blade.php
@@ -6,7 +6,7 @@
             <div class="row ct-u-paddingTop100">
                 <div class="col-md-12 ct-titleBox">
                     <h4 class="text-center text-uppercase ct-u-paddingTop30">
-                        Actualizar a: <i>{{ $dancer->fullName }}</i>
+                        Registrar categoría
                     </h4>
                 </div>
             </div>
@@ -17,18 +17,17 @@
             </div>
             <div class="row ct-u-paddingTop25">
                 <div class="col-md-offset-2 col-md-8">
-                    {!! Form::model($dancer,
-                                    array(
-                                        'url' => route('pluranza.dancers.update', $dancer->id),
-                                        'method' => 'PATCH',
+                    {!! Form::open(array(
+                                        'url' => route('categories.store'),
+                                        'method' => 'post',
                                         'accept-charset' => 'UTF-8',
                                         'role' => 'form',
                                         'files' => true
                                         )) !!}
-                        @include('pluranza.dancers.partials._form')
+                        @include('categories.partials._form')
                         <div class="row">
                             <div class="col-sm-offset-9 col-sm-3">
-                                {!! Form::submit('Actualizar', [ 'class' => 'btn btn-xs btn-primary btn-block text-uppercase ct-u-size14']) !!}
+                                {!! Form::submit('Guardar', [ 'class' => 'btn btn-xs btn-primary btn-block text-uppercase ct-u-size14']) !!}
                             </div>
                         </div>
                     {!! Form::close() !!}
