e4df12d5d28d513d10c86b0fcbc93ed51e40b126
Lenyn Alcántara <nightzpy@gmail.com>
2015-12-25 08:01:18 +0000

    Agregados estilos para mostrar pagos en vista de competidores por academia y boton para ver todos los pagos por academia

--- resources/views/pluranza/competitors/by-academy.blade.php
+++ resources/views/pluranza/competitors/by-academy.blade.php
@@ -19,18 +19,46 @@
             @if($competitionTypes)
                 <div class="row ct-u-paddingTop10">
                     <div class="col-md-offset-2 col-md-8">
-                        <div class="col-md-3">
-                            <p>Debe: <i>{{ $academy->debtBs }}</i></p>
+                        <div class="col-md-4">
+                            <div class="text-center ct-pricingBox ct-pricingBox--standard ct-pricingBox--important">
+                                <div class="ct-pricingBox-title">
+                                    <h5 class="text-uppercase">Debe</h5>
+                                </div>
+                                <div class="ct-pricingBox-pricing">
+                                    <span class="ct-pricingBox-price">{{ $academy->debtBs }}</span>
+                                </div>
+                            </div>
                         </div>
                         <div class="col-md-4">
-                            <p>Cancelado: <i>{{ $academy->paidBs }}</i></p>
+                            <div class="text-center ct-pricingBox ct-pricingBox--standard ct-pricingBox--important">
+                                <div class="ct-pricingBox-title">
+                                    <h5 class="text-uppercase">Cancelado</h5>
+                                </div>
+                                <div class="ct-pricingBox-pricing">
+                                    <span class="ct-pricingBox-price">{{ $academy->paidBs }}</span>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-md-3">
-                            <p>Total: <i>{{ $academy->totalBs }}</i></p>
+                        <div class="col-md-4">
+                            <div class="text-center ct-pricingBox ct-pricingBox--standard ct-pricingBox--standart">
+                                <div class="ct-pricingBox-title">
+                                    <h5 class="text-uppercase">Total</h5>
+                                </div>
+                                <div class="ct-pricingBox-pricing">
+                                    <span class="ct-pricingBox-price">{{ $academy->totalBs }}</span>
+                                </div>
+                                <div class="btn-group text-center ct-u-paddingTop15" data-toggle="buttons" role="group">
+                                    <a href="{{ route('pluranza.payments.new', $academy->id) }}" class="ct-js-btnScroll btn btn-xs btn-danger btn-circle text-uppercase ct-u-size14 pull-left"><i class="fa fa-money fa-2x"></i> Pagar</a>
+                                    <a href="{{ route('pluranza.payments.by-academy', $academy->id) }}" class="ct-js-btnScroll btn btn-xs btn-danger btn-circle text-uppercase ct-u-size14 pull-left"><i class="fa fa-money fa-2x"></i> Pagos</a>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-md-2">
-                            <a href="{{ route('pluranza.payments.new', $academy->id) }}" class="ct-js-btnScroll btn btn-xs btn-danger btn-circle text-uppercase ct-u-size14 pull-left"><i class="fa fa-money fa-2x"></i> Pagar</a>
-                        </div>
+                        {{--<div class="col-md-4">
+                            <div class="btn-group pull-left" data-toggle="buttons" role="group">
+                                <a href="{{ route('pluranza.payments.new', $academy->id) }}" class="ct-js-btnScroll btn btn-xs btn-danger btn-circle text-uppercase ct-u-size14 pull-left"><i class="fa fa-money fa-2x"></i> Pagar</a>
+                                <a href="{{ route('pluranza.payments.by-academy', $academy->id) }}" class="ct-js-btnScroll btn btn-xs btn-danger btn-circle text-uppercase ct-u-size14 pull-left"><i class="fa fa-money fa-2x"></i> Pagos</a>
+                            </div>
+                        </div>--}}
                     </div>
                 </div>
                 <div class="row ct-u-paddingTop25">
