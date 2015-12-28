8dc229f058ed74c2da55e93c94f7b168de6a2b12
Lenyn Alcántara <nightzpy@gmail.com>
2015-12-28 01:43:20 +0000

    Añadido menú configuración según rol admin

--- resources/views/pluranza/admin/partials/_navbar-onepage.blade.php
+++ resources/views/pluranza/admin/partials/_navbar-onepage.blade.php
@@ -13,10 +13,34 @@
                 <div class="col-lg-8 col-md-7 col-sm-6">
                     <ul class="nav navbar-nav navbar-right ct-navbar--fadeIn">
                         <li class="onepage"><a href="{{ route('pluranza.home') }}" class="ct-js-btnScroll">Inicio</a></li>
-                        <li class="onepage"><a href="{{ route('pluranza.academies.edit', $academy->id) }}" class="ct-js-btnScroll">Editar</a></li>
-                        <li class="onepage"><a href="{{ route('pluranza.dancers.by-academy', $academy->id) }}" class="ct-js-btnScroll">Miembros</a></li>
-                        <li class="onepage"><a href="{{ route('pluranza.competitors.by-academy', $academy->id) }}" class="ct-js-btnScroll">Competidores</a></li>
-                        <li class="onepage"><a href="#" class="ct-js-btnScroll">Resultados</a></li>
+
+                        @if(Entrust::hasRole('director'))
+                            <li class="onepage"><a href="{{ route('pluranza.academies.edit', $academy->id) }}" class="ct-js-btnScroll">Editar</a></li>
+                            <li class="onepage"><a href="{{ route('pluranza.dancers.by-academy', $academy->id) }}" class="ct-js-btnScroll">Miembros</a></li>
+                            <li class="onepage"><a href="{{ route('pluranza.competitors.by-academy', $academy->id) }}" class="ct-js-btnScroll">Competidores</a></li>
+                            <li class="onepage"><a href="#" class="ct-js-btnScroll">Resultados</a></li>
+                        @endif
+
+                        @if(Entrust::hasRole('admin'))
+                            <li class="dropdown" role="presentation">
+                                <a href="staff.html" class="dropdown-toggle">Academias<b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="{{ route('categories.home') }}">Miembros</a></li>
+                                    <li><a href="{{ route('levels.home') }}">Competidores</a></li>
+                                    <li><a href="{{ route('pluranza.competition-types.home') }}">Tipos de competencias</a></li>
+                                    <li><a href="{{ route('pluranza.competition-categories.home') }}">Categorías en competencia</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown" role="presentation">
+                                <a href="#" class="dropdown-toggle">Configuración<b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="{{ route('categories.home') }}">Categorías</a></li>
+                                    <li><a href="{{ route('levels.home') }}">Niveles</a></li>
+                                    <li><a href="{{ route('pluranza.competition-types.home') }}">Tipos de competencias</a></li>
+                                    <li><a href="{{ route('pluranza.competition-categories.home') }}">Categorías en competencia</a></li>
+                                </ul>
+                            </li>
+                        @endif
                     </ul>
                     <div class="clearfix"></div>
                 </div>
