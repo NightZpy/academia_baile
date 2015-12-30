45baf3557ed02ba6659f7b154dcfee7fbdcea7e0
Lenyn Alcántara <nightzpy@gmail.com>
2015-12-30 16:01:30 +0000

    Arreglado formulario de competidores

--- resources/views/pluranza/competitors/partials/_form.blade.php
+++ resources/views/pluranza/competitors/partials/_form.blade.php
@@ -1,3 +1,6 @@
+<div class="row">
+    <h5 class="text-center ct-titleBox">Obligatorio (*)</h5>
+</div>
 {!! Form::hidden('academy_id', $academy->id) !!}
 {!! Form::hidden('competition_type_id',  isset($competitor) ? $competitor->competitionType->id : $competitionType->id) !!}
 <div class="row">
@@ -12,6 +15,9 @@
                     </ul>
                 </label>
             @endif
+            <label class="control-label" for="song">
+                Nombre con el que se identificarán (*)
+            </label>
             {!! Form::text('name', (isset($name) ? $name : old('name')), array('placeholder' => 'Nombre', 'class' => 'form-control input-sm')) !!}
         </div>
     </div>
@@ -28,6 +34,9 @@
                     </ul>
                 </label>
             @endif
+            <label class="control-label" for="song">
+                Género de baile (*)
+            </label>
             {!! Form::select('category_id', (isset($categories) ? $categories : array()), ( isset($competitor) AND $competitor->category->id > 0 ? $competitor->category->id : old('category_id')), ['placeholder' => 'Selecciona un género', 'class' => 'form-control input-sm category-select']) !!}
         </div>
     </div>
@@ -44,6 +53,9 @@
                     </ul>
                 </label>
             @endif
+            <label class="control-label" for="song">
+                Nivel de competición (*)
+            </label>
             {!! Form::select('level_id', (isset($levels) ? $levels : array()), ( isset($competitor) AND $competitor->level->id > 0 ? $competitor->level->id : old('level_id')), ['placeholder' => 'Selecciona un nivel', 'class' => 'form-control input-sm level-select']) !!}
         </div>
     </div>
@@ -61,11 +73,14 @@
                         </ul>
                     </label>
                 @endif
+                <label class="control-label" for="song">
+                    Bailarina (*)
+                </label>
                 {!! Form::select('dancer_id[female]', (isset($dancers['female']) ? $dancers['female'] : array()), ( isset($competitor) AND $competitor->dancers()->female()->count() > 0 ? $competitor->dancers()->female()->pluck('id') : old('dancer_id[female]')), ['placeholder' => 'Selecciona bailarina', 'class' => 'form-control input-sm', 'required' => 'required']) !!}
             </div>
         </div>
-</div>
-<div class="row">
+    </div>
+    <div class="row">
     <div class="col-sm-offset-4 col-sm-4">
         <div class="form-group {{ ($errors->has('dancer_id[masculine]') ? 'has-error' : '') }}">
             @if ($errors->has('dancer_id'))
@@ -77,6 +92,9 @@
                     </ul>
                 </label>
             @endif
+            <label class="control-label" for="song">
+                Bailarín (*)
+            </label>
             {!! Form::select('dancer_id[masculine]', (isset($dancers['masculine']) ? $dancers['masculine'] : array()), ( isset($competitor) AND $competitor->dancers()->masculine()->count() > 0 ? $competitor->dancers()->masculine()->pluck('id') : old('dancer_id[masculine]')), ['placeholder' => 'Selecciona bailarín', 'class' => 'form-control input-sm', 'required' => 'required']) !!}
         </div>
     </div>
@@ -92,9 +110,15 @@
                         </ul>
                     </label>
                 @endif
-                @if ((isset($competitor) AND strtolower($competitor->competitionType->name) == 'pareja') OR  strtolower($competitionType->name) == 'solista')
+                @if ((isset($competitor) AND strtolower($competitor->competitionType->name) == 'solista') OR  strtolower($competitionType->name) == 'solista')
+                    <label class="control-label" for="song">
+                        Bailarín o Bailarina (*)
+                    </label>
                     {!! Form::select('dancer_id[]', (isset($dancers) ? $dancers : array()), ( isset($dancerId) AND $dancerId > 0 ? $dancerId : old('dancer_id')), ['placeholder' => 'Selecciona los bailarines', 'class' => 'form-control input-sm', 'required' => 'required']) !!}
                 @else
+                    <label class="control-label" for="song">
+                        Bailarines (*)
+                    </label>
                     {!! Form::select('dancer_id[]', (isset($dancers) ? $dancers : array()), ( isset($dancerId) AND $dancerId > 0 ? $dancerId : old('dancer_id')), ['multiple' => 'multiple', 'placeholder' => 'Selecciona los bailarines', 'class' => 'form-control input-sm', 'required' => 'required']) !!}
                 @endif
             </div>
@@ -114,7 +138,7 @@
                 </label>
             @endif
             <label class="control-label" for="song">
-                Nombre de la canción (*)
+                Nombre de la canción
             </label>
             {!! Form::text('song_name', (isset($song_name) ? $song_name : old('song_name')), array('placeholder' => 'Nombre del a canción', 'class' => 'form-control input-sm')) !!}
         </div>
