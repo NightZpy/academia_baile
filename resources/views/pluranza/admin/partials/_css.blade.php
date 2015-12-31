12d72b42bc5bb971438c78801f5dc44b6c6b835d
Lenyn Alcántara <nightzpy@gmail.com>
2015-12-31 16:25:11 +0000

    Eliminados js y css innecesarios

--- resources/views/pluranza/admin/partials/_css.blade.php
+++ resources/views/pluranza/admin/partials/_css.blade.php
@@ -5,14 +5,14 @@
 <!-- DATATABLES -->
 
 {!! HTML::style('/assets/plugins/datatables/DataTables-1.10.10/css/dataTables.bootstrap.min.css') !!}
-{!! HTML::style('/assets/plugins/datatables/AutoFill-2.1.0/css/autoFill.bootstrap.css') !!}
+{{--{!! HTML::style('/assets/plugins/datatables/AutoFill-2.1.0/css/autoFill.bootstrap.css') !!}
 {!! HTML::style('/assets/plugins/datatables/Buttons-1.1.0/css/buttons.bootstrap.min.css') !!}
 {!! HTML::style('/assets/plugins/datatables/FixedColumns-3.2.0/css/fixedColumns.bootstrap.min.css') !!}
 {!! HTML::style('/assets/plugins/datatables/FixedHeader-3.1.0/css/fixedHeader.bootstrap.min.css') !!}
-{!! HTML::style('/assets/plugins/datatables/KeyTable-2.1.0/css/keyTable.bootstrap.min.css') !!}
+{!! HTML::style('/assets/plugins/datatables/KeyTable-2.1.0/css/keyTable.bootstrap.min.css') !!}--}}
 {!! HTML::style('/assets/plugins/datatables/Responsive-2.0.0/css/responsive.bootstrap.min.css') !!}
-{!! HTML::style('/assets/plugins/datatables/RowReorder-1.1.0/css/rowReorder.bootstrap.min.css') !!}
-{!! HTML::style('/assets/plugins/datatables/Scroller-1.4.0/css/scroller.bootstrap.min.css') !!}
+{{--{!! HTML::style('/assets/plugins/datatables/RowReorder-1.1.0/css/rowReorder.bootstrap.min.css') !!}
+{!! HTML::style('/assets/plugins/datatables/Scroller-1.4.0/css/scroller.bootstrap.min.css') !!}--}}
 
 {!! Html::style('/assets/css/style.css') !!}
 {!! Html::style('/assets/css/salsa.css') !!}
--- resources/views/pluranza/admin/partials/_js.blade.php
+++ resources/views/pluranza/admin/partials/_js.blade.php
@@ -8,12 +8,12 @@
 {!! HTML::script('/assets/plugins/fileinput/fileinput_locale_es.js') !!}
 
 <!-- DATATABLES -->
-{!! HTML::script('/assets/plugins/datatables/JSZip-2.5.0/jszip.min.js') !!}
+{{--{!! HTML::script('/assets/plugins/datatables/JSZip-2.5.0/jszip.min.js') !!}
 {!! HTML::script('/assets/plugins/datatables/pdfmake-0.1.18/build/pdfmake.min.js') !!}
-{!! HTML::script('/assets/plugins/datatables/pdfmake-0.1.18/build/vfs_fonts.js') !!}
+{!! HTML::script('/assets/plugins/datatables/pdfmake-0.1.18/build/vfs_fonts.js') !!}--}}
 {!! HTML::script('/assets/plugins/datatables/DataTables-1.10.10/js/jquery.dataTables.min.js') !!}
 {!! HTML::script('/assets/plugins/datatables/DataTables-1.10.10/js/dataTables.bootstrap.min.js') !!}
-{!! HTML::script('/assets/plugins/datatables/AutoFill-2.1.0/js/dataTables.autoFill.min.js') !!}
+{{--{!! HTML::script('/assets/plugins/datatables/AutoFill-2.1.0/js/dataTables.autoFill.min.js') !!}
 {!! HTML::script('/assets/plugins/datatables/AutoFill-2.1.0/js/autoFill.bootstrap.min.js') !!}
 {!! HTML::script('/assets/plugins/datatables/Buttons-1.1.0/js/dataTables.buttons.min.js') !!}
 {!! HTML::script('/assets/plugins/datatables/Buttons-1.1.0/js/buttons.bootstrap.min.js') !!}
@@ -22,10 +22,10 @@
 {!! HTML::script('/assets/plugins/datatables/Buttons-1.1.0/js/buttons.print.min.js') !!}
 {!! HTML::script('/assets/plugins/datatables/FixedColumns-3.2.0/js/dataTables.fixedColumns.min.js') !!}
 {!! HTML::script('/assets/plugins/datatables/FixedHeader-3.1.0/js/dataTables.fixedHeader.min.js') !!}
-{!! HTML::script('/assets/plugins/datatables/KeyTable-2.1.0/js/dataTables.keyTable.min.js') !!}
+{!! HTML::script('/assets/plugins/datatables/KeyTable-2.1.0/js/dataTables.keyTable.min.js') !!}--}}
 {!! HTML::script('/assets/plugins/datatables/Responsive-2.0.0/js/dataTables.responsive.min.js') !!}
 {!! HTML::script('/assets/plugins/datatables/Responsive-2.0.0/js/responsive.bootstrap.min.js') !!}
-{!! HTML::script('/assets/plugins/datatables/RowReorder-1.1.0/js/dataTables.rowReorder.min.js') !!}
+{{--{!! HTML::script('/assets/plugins/datatables/RowReorder-1.1.0/js/dataTables.rowReorder.min.js') !!}--}}
 
 {!! Html::script('/assets/js/custom.js') !!}
 
