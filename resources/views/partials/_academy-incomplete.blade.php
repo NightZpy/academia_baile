7d7af86d4883ab3b5f51169610bdd720b4da9eed
Lenyn Alcántara <nightzpy@gmail.com>
2016-01-02 04:10:13 -0430

    Cambio de mensaje de bienvenida al login

--- resources/views/partials/_academy-incomplete.blade.php
+++ resources/views/partials/_academy-incomplete.blade.php
@@ -1,6 +1,6 @@
-{{--@if (isset($academy) AND !$academy->isDataComplete)--}}
+@if (isset($academy) AND !$academy->isDataComplete)
     <div class="alert alert-danger text-center">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-        ¡Los datos de su academia no están completos, puedes llenar los restantes <a href="{{ route('pluranza.academies.edit', $academy->id) }}" class="ct-js-btnScroll">AQUÍ</a>.
+        Los datos de su academia no están completos, puedes llenar los restantes <a href="{{ route('pluranza.academies.edit', $academy->id) }}" class="ct-js-btnScroll">AQUÍ</a>.
     </div>
-{{--@endif--}}
+@endif
