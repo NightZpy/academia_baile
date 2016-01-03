fc1ae7c95280e4b50f63b0539776d5288c64f33b
Lenyn Alcántara <nightzpy@gmail.com>
2016-01-03 02:50:26 +0000

    Agregando imagen nueva para el scraping de facebook

--- resources/views/public/layout/includes/_head.blade.php
+++ resources/views/public/layout/includes/_head.blade.php
@@ -8,9 +8,18 @@
 <link rel="apple-touch-icon" href="/favicon-alcompas.png">
 <!-- FACEBOOK TAGS -->
 <meta property="fb:profile_id" content="100006770895642" />
-<meta property="og:title" content="Al Compás - Academia de Baile" />
-<meta property="og:description" content="Página web de Al Compás Academia de Baile" />
-<meta property="og:site_name" content="Al Compás" />
+@if(isset($configuration))
+    <title>{{ $configuration->title }}</title>
+    <meta name="description" content="{{ $configuration->description }}">
+    <!-- FACEBOOK TAGS -->
+    <meta property="og:title" content="{{ $configuration->title }}" />
+    <meta property="og:description" content="{{ $configuration->description }}" />
+    <meta property="og:site_name" content="{{ $configuration->long_title }}" />
+@else
+    <meta property="og:title" content="Al Compás - Academia de Baile" />
+    <meta property="og:description" content="Página web de Al Compás Academia de Baile" />
+    <meta property="og:site_name" content="Al Compás" />
+@endif
 <meta property="og:type" content="website" />
 <meta property="og:image" content="{{ asset('/assets/images/content/slider/pluranza-facebook.jpg') }}" />
 <meta property="og:image" content="{{ asset('/assets/images/content/slider/alcompas.png') }}" />
