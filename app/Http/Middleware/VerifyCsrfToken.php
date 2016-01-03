d36170f6d133e4a4bca1fa85a00a98fd2a4e4135
Lenyn Alcántara <nightzpy@gmail.com>
2016-01-03 09:58:31 +0000

    Probando url

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
+        'pluranza/competidores'
     ];
 
     /**
@@ -24,11 +25,10 @@ class VerifyCsrfToken extends BaseVerifier
      */
     public function handle($request, Closure $next)
     {
-        array_push($this->except, route('pluranza.competitors.store'));
         \Debugbar::info($request);
         \Debugbar::info($request->file('song'));
         \Debugbar::info($request->hasFile('song'));
-        \Debugbar::info(['Request is: ' => $request->is(route('pluranza.competitors.store'))]);
+        \Debugbar::info(['Request is: ' => $request->is('pluranza/competidores'))]);
         \Debugbar::info($this->except);
         if ($this->isReading($request) || $this->tokensMatch($request)) {
             \Debugbar::info($request->file('song'));
