ec6afbad4509147719dad9d527f74e0fe7dfd0e5
Lenyn Alcántara <nightzpy@gmail.com>
2016-01-08 03:55:27 +0000

    Cambio estilo jurados public

--- resources/views/pluranza/jurors/public.blade.php
+++ resources/views/pluranza/jurors/public.blade.php
@@ -29,26 +29,12 @@
             </div>
             <div class="row ct-u-paddingTop30">
                 <div class="col-md-12">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="ct-artisticBox">
-                                {{-- <div class="ct-artisticBox-content">
-                                    <div class="ct-backgroundContent" data-bg-color="#f7f7f7" data-type="color">
-                                        <div class="ct-artisticBox-description">
-                                            <h3 class="ct-artisticBox-date">2014 - 2015</h3>
-                                            <h4 class="ct-artisticBox-title text-uppercase">Artistic Staff</h4>
-                                            <ul class="list-unstyled ct-artistBox-members">
-                                                <li>Karen Jenkins - <a href="staff.html#">Ballet Master</a></li>
-                                                <li>Lisa Donalds - <a href="staff.html">Ballet Mistress</a></li>
-                                                <li>Dana Johnsons - <a href="staff.html#">Rehearsal Director</a></li>
-                                                <li>Melissa Kens - <a href="staff.html#">Artistic Director</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div> --}}
-                                <div class="ct-artisticBox-left ct-artisticBox-personImage">
-                                    @foreach ($category->jurors as $jury)
-                                    <a href="{{ route ('pluranza.jurors.show', $jury->id) }}" class="ct-personBox ct-personBox--primary ct-js-btnScroll">
+                    <div class="row ct-u-paddingTop30 ct-u-paddingBottom70">
+                        <div class="ct-js-owl ct-u-owl ct-twoLinesCarousel ct-carouselNavigation--arrowsTopRight owl-carousel owl-theme" data-single="false" data-navigation="true" data-pagination="false" data-items="5" data-autoplay="false" data-snap-ignore="true" style="opacity: 1; display: block;">
+                            <div class="owl-wrapper-outer"><div class="owl-wrapper" style="width: 2736px; left: 0px; display: block; transform: translate3d(0px, 0px, 0px);"><div class="owl-item active" style="width: 228px;"><div class="item"><!-- Item 2A, Item 2B -->
+                            <div class="ct-owlCarousel-twoLinesWrapper">
+                                @foreach ($category->jurors as $jury)
+                                    <a href="{{ route ('pluranza.jurors.show', $jury->id) }}" class="ct-personBox ct-personBox--primary ct-js-btnScroll personBox-a-margin-side">
                                         <figure class="ct-personBox-image">
                                             <img src="{{ $jury->photo->url('public') }}" alt="{{ $jury->fullName }}">
                                             <figcaption>
@@ -57,11 +43,13 @@
                                             </figcaption>
                                         </figure>
                                     </a>
-                                    @endforeach
-                                </div>
-                                <div class="clearfix"></div>
+                                @endforeach
+                            </div>
                             </div>
                         </div>
+                        </div>
+                        </div>
+                        <div class="owl-controls clickable"><div class="owl-buttons"><div class="owl-prev"></div><div class="owl-next"></div></div></div></div>
                     </div>
                 </div>
             </div>
