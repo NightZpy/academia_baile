4d967a15ada96be6d2571748791d48003f39dbf7
Lenyn Alcántara <nightzpy@gmail.com>
2016-01-08 04:00:44 +0000

    Cambio estilo jurados public

--- resources/views/pluranza/jurors/public.blade.php
+++ resources/views/pluranza/jurors/public.blade.php
@@ -30,26 +30,29 @@
             <div class="row ct-u-paddingTop30">
                 <div class="col-md-12">
                     <div class="row ct-u-paddingTop30 ct-u-paddingBottom70">
-                        <div class="ct-js-owl ct-u-owl ct-twoLinesCarousel ct-carouselNavigation--arrowsTopRight owl-carousel owl-theme" data-single="false" data-navigation="true" data-pagination="false" data-items="5" data-autoplay="false" data-snap-ignore="true" style="opacity: 1; display: block;">
-                            <div class="owl-wrapper-outer"><div class="owl-wrapper" style="width: 2736px; left: 0px; display: block; transform: translate3d(0px, 0px, 0px);"><div class="owl-item active" style="width: 228px;"><div class="item"><!-- Item 2A, Item 2B -->
-                            <div class="ct-owlCarousel-twoLinesWrapper">
-                                @foreach ($category->jurors as $jury)
-                                    <a href="{{ route ('pluranza.jurors.show', $jury->id) }}" class="ct-personBox ct-personBox--primary ct-js-btnScroll personBox-a-margin-side">
-                                        <figure class="ct-personBox-image">
-                                            <img src="{{ $jury->photo->url('public') }}" alt="{{ $jury->fullName }}">
-                                            <figcaption>
-                                            <div class="ct-personBox-name">{{ $jury->fullName }}</div>
-                                            <span class="ct-personBox-linkHelper">Ver</span>
-                                            </figcaption>
-                                        </figure>
-                                    </a>
-                                @endforeach
+                        <div class="ct-js-owl ct-u-owl ct-twoLinesCarousel ct-carouselNavigation--arrowsTopRight owl-carousel owl-theme" data-single="false" data-navigation="true" data-pagination="false" data-items="5" data-autoplay="false" data-snap-ignore="true">
+                            <div class="owl-wrapper-outer">
+                                <div class="owl-wrapper">
+                                    @foreach ($category->jurors as $jury)
+                                    <div class="owl-item">
+                                        <div class="item"><!-- Item 2A, Item 2B -->
+                                            <div class="ct-owlCarousel-twoLinesWrapper">
+                                                <a href="{{ route ('pluranza.jurors.show', $jury->id) }}" class="ct-personBox ct-personBox--primary ct-js-btnScroll personBox-a-margin-side">
+                                                    <figure class="ct-personBox-image">
+                                                        <img src="{{ $jury->photo->url('public') }}" alt="{{ $jury->fullName }}">
+                                                        <figcaption>
+                                                        <div class="ct-personBox-name">{{ $jury->fullName }}</div>
+                                                        <span class="ct-personBox-linkHelper">Ver</span>
+                                                        </figcaption>
+                                                    </figure>
+                                                </a>                                                
+                                            </div>
+                                        </div>
+                                    </div>
+                                    @endforeach
+                                </div>
                             </div>
-                            </div>
                         </div>
-                        </div>
-                        </div>
-                        <div class="owl-controls clickable"><div class="owl-buttons"><div class="owl-prev"></div><div class="owl-next"></div></div></div></div>
                     </div>
                 </div>
             </div>
