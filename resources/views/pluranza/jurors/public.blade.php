5c2c7f6fb07a3db4499f2f5507854e1917347036
Lenyn Alcántara <nightzpy@gmail.com>
2016-01-08 04:06:48 +0000

    Cambio estilo jurados public

--- resources/views/pluranza/jurors/public.blade.php
+++ resources/views/pluranza/jurors/public.blade.php
@@ -31,27 +31,20 @@
                 <div class="col-md-12">
                     <div class="row ct-u-paddingTop30 ct-u-paddingBottom70">
                         <div class="ct-js-owl ct-u-owl ct-twoLinesCarousel ct-carouselNavigation--arrowsTopRight owl-carousel owl-theme" data-single="false" data-navigation="true" data-pagination="false" data-items="5" data-autoplay="false" data-snap-ignore="true">
-                            <div class="owl-wrapper-outer">
-                                <div class="owl-wrapper">
-                                    @foreach ($category->jurors as $jury)
-                                    <div class="owl-item">
-                                        <div class="item"><!-- Item 2A, Item 2B -->
-                                            <div class="ct-owlCarousel-twoLinesWrapper">
-                                                <a href="{{ route ('pluranza.jurors.show', $jury->id) }}" class="ct-personBox ct-personBox--primary ct-js-btnScroll personBox-a-margin-side">
-                                                    <figure class="ct-personBox-image">
-                                                        <img src="{{ $jury->photo->url('public') }}" alt="{{ $jury->fullName }}">
-                                                        <figcaption>
-                                                        <div class="ct-personBox-name">{{ $jury->fullName }}</div>
-                                                        <span class="ct-personBox-linkHelper">Ver</span>
-                                                        </figcaption>
-                                                    </figure>
-                                                </a>                                                
-                                            </div>
-                                        </div>
-                                    </div>
-                                    @endforeach
-                                </div>
+
+                            <div class="owl-item active" style="width: 228px;"><div class="item"><!-- Item 1A, Item 1B -->
+                            <div class="ct-owlCarousel-twoLinesWrapper">
+                                <a href="{{ route ('pluranza.jurors.show', $jury->id) }}" class="ct-personBox ct-personBox--primary ct-js-btnScroll personBox-a-margin-side">
+                                    <figure class="ct-personBox-image">
+                                        <img src="{{ $jury->photo->url('public') }}" alt="{{ $jury->fullName }}">
+                                        <figcaption>
+                                        <div class="ct-personBox-name">{{ $jury->fullName }}</div>
+                                        <span class="ct-personBox-linkHelper">Ver</span>
+                                        </figcaption>
+                                    </figure>
+                                </a>
                             </div>
+                            </div></div>
                         </div>
                     </div>
                 </div>
