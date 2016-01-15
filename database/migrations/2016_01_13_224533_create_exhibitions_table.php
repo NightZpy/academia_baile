977bdbebb54ddf839e5dd9388a418c27473bf53a
Lenyn Alcántara <nightzpy@gmail.com>
2016-01-15 04:13:26 +0000

    Arreglado problema con migración de exhibiciones

--- database/migrations/2016_01_13_224533_create_exhibitions_table.php
+++ database/migrations/2016_01_13_224533_create_exhibitions_table.php
@@ -17,9 +17,9 @@ class CreateExhibitionsTable extends Migration
             $table->string('name', 128);
             $table->string('song_name', 128)->nullable();
             $table->string('song', 128)->nullable();
-            $table->integer('evend_edition_id');
-            $table->foreign('evend_edition_id')->references('id')->on('evend_editions');
-            $table->integer('academy_id');
+            $table->integer('event_edition_id')->unsigned();
+            $table->foreign('event_edition_id')->references('id')->on('event_editions');
+            $table->integer('academy_id')->unsigned();
             $table->foreign('academy_id')->references('id')->on('academies');
             $table->unique(['academy_id', 'event_edition_id', 'name'], 'exhibitions_unique_key');
             $table->timestamps();
