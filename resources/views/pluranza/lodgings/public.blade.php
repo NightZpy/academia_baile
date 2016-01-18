054152ea097fdd2216f2a237341b5e07276615a2
Lenyn Alcántara <nightzpy@gmail.com>
2016-01-18 15:44:35 +0000

    Acomodando párrafo address

--- resources/views/pluranza/lodgings/public.blade.php
+++ resources/views/pluranza/lodgings/public.blade.php
@@ -17,9 +17,9 @@
                         <ul class="ct-list--paddingLeft15">
                             <li><strong>Télefonos:</strong> <i>{{ $lodging->phones }}</i></li>
                             <li><strong>Web:</strong> <i><a href="{{ $lodging->web }}">{{ $lodging->web }}</a></i></li>
-                            <li><strong>Dirección:</strong> <i><pre>
+                            <li><strong>Dirección:</strong> <i><p>
                                 {{ $lodging->address }}</i>
-                            </pre></li>
+                            </p></li>
                         </ul>
                     </div>
                 </div>
