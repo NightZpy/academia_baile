f9212b002953ae6065d7270fdbdcc6b3e723fa91
Lenyn Alcántara <nightzpy@gmail.com>
2016-01-22 01:53:34 -0430

    Instalando tracker

--- config/database.php
+++ config/database.php
@@ -64,6 +64,18 @@ return [
             'strict'    => false,
         ],
 
+        'tracker' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE_TRACKER', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+
         'pgsql' => [
             'driver'   => 'pgsql',
             'host'     => env('DB_HOST', 'localhost'),
@@ -83,7 +95,7 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'charset'  => 'utf8',
             'prefix'   => '',
-        ],
+        ],        
 
     ],
 
