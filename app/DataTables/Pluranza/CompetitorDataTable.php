a83b99916c380ab8e01d0125760afaa89370d62d
Lenyn Alcántara <nightzpy@gmail.com>
2016-04-16 05:54:28 -0430

    Mostrando competidores en orden desc por fecha de agregado

--- app/DataTables/Pluranza/CompetitorDataTable.php
+++ app/DataTables/Pluranza/CompetitorDataTable.php
@@ -22,7 +22,8 @@ class CompetitorDataTable extends BaseDataTable
 		$this->setRoute('pluranza.competitors.api.list');
 		$this->setOrderColumn(0);
 		$this->setOrderType('desc');
-		$this->setHideColumns([0]);
+		if (!Entrust::hasRole('admin'))
+			$this->setHideColumns([0]);
 
 		if (!Entrust::hasRole('director'))
 			$this->columns = ['Academia'];
